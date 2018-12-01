a616c9fd70fd716466a9696911c19f02cb9bdf19
Erick Gannem <erickgannem@gmail.com>
2018-12-01 15:00:31 -0300

    main.js edited

--- js/main.js
+++ js/main.js
@@ -1,39 +1,37 @@
 'use strict';
 
-window.addEventListener('DOMContentLoaded', () => {
- const DOMElements = {
-  'whatsAppButton': document.querySelector('.whatsapp-button'),
-  'heroContainer': document.querySelector('.hero-container'),
-  'contactSection': document.querySelector('.contact'),
-  'navbar': document.querySelector('.navbar'),
-  'serviceDescription': Array.from(document.querySelectorAll('.service-description'))
+const DOMElements = {
+ 'whatsAppButton': document.querySelector('.whatsapp-button'),
+ 'heroContainer': document.querySelector('.hero-container'),
+ 'contactSection': document.querySelector('.contact'),
+ 'navbar': document.querySelector('.navbar'),
+ 'serviceDescription': Array.from(document.querySelectorAll('.service-description'))
+}
+// Click Event Handler
+window.addEventListener('click', e => {
+ const eventTarget = e.target;
+
+ if(eventTarget.matches('.whatsapp-button')) {
+  redirectWhatsapp(DOMElements);
  }
- // Click Event Handler
- window.addEventListener('click', e => {
-  const eventTarget = e.target;
-
-  if(eventTarget.matches('.whatsapp-button')) {
-   redirectWhatsapp(DOMElements);
-  }
-  if(eventTarget.matches('.nav-contact') || eventTarget.matches('.navbar-link-button')) {
-   scrollToContact(DOMElements);
-  }
-  if(eventTarget.matches('.go-top') || eventTarget.matches('footer') || eventTarget.matches('.nav-home') || eventTarget.matches('.fa-home')) {
-   scrollToTop(DOMElements);
-  }
- });
- // Scroll Event Handler
- window.addEventListener('scroll', e => {
-  wAppAndNavStyling(DOMElements);
- });
-
- // Resize Event Handler
- window.addEventListener('resize', e => {
-  if(window.innerHeight > 820) {
-   fixServicesHeight(DOMElements);
-  }
- })
+ if(eventTarget.matches('.nav-contact') || eventTarget.matches('.navbar-link-button')) {
+  scrollToContact(DOMElements);
+ }
+ if(eventTarget.matches('.go-top') || eventTarget.matches('footer') || eventTarget.matches('.nav-home') || eventTarget.matches('.fa-home')) {
+  scrollToTop(DOMElements);
+ }
 });
+// Scroll Event Handler
+window.addEventListener('scroll', e => {
+ wAppAndNavStyling(DOMElements);
+});
+
+// Resize Event Handler
+window.addEventListener('resize', e => {
+ if(window.innerHeight > 820) {
+  fixServicesHeight(DOMElements);
+ }
+})
 
 function redirectWhatsapp() {
  const num = 556699331539;

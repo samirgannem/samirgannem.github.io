206db953b9330704f4ae0c2231dfe9b4027bc6b4
Erick Gannem <erickgannem@gmail.com>
2019-12-14 12:48:27 -0400

    location changed

--- js/map.js
+++ js/map.js
@@ -4,9 +4,10 @@ const mapEl = document.querySelector('.map');
 
 function initMap() {
  const location = {
-  lat: -15.551626,
-  lng: -55.154645
+  lat:  -15.550027,
+  lng: -55.164853
  };
+
  const map = new google.maps.Map(mapEl, {
   zoom: 16,
   center: location

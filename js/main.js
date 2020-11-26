c8ea5b05b35d7a3602f2708cf87f121b5a5e24f4
Erick Gannem <erickgannem@gmail.com>
2020-11-25 23:58:41 -0400

    implementing contact form

--- js/main.js
+++ js/main.js
@@ -1,98 +1,101 @@
-'use strict';
-
 const DOMElements = {
- 'whatsAppButton': document.querySelector('.whatsapp-button'),
- 'heroContainer': document.querySelector('.hero-container'),
- 'contactSection': document.querySelector('.contact'),
- 'navbar': document.querySelector('.navbar'),
- 'serviceDescription': Array.from(document.querySelectorAll('.service-description')),
- 'navbarBrandTitle': document.querySelector('.navbar-brand-title'),
- 'navbarBrandImage': document.querySelector('.navbar-brand-image'),
- 'container': document.querySelector('.container'),
- 'progressBar': document.querySelector('.progress-bar'),
- 'heroHeader': document.querySelector('.hero-header'),
-}
+  whatsAppButton: document.querySelector('.whatsapp-button'),
+  heroContainer: document.querySelector('.hero-container'),
+  contactSection: document.querySelector('.contact'),
+  navbar: document.querySelector('.navbar'),
+  serviceDescription: Array.from(document.querySelectorAll('.service-description')),
+  navbarBrandTitle: document.querySelector('.navbar-brand-title'),
+  navbarBrandImage: document.querySelector('.navbar-brand-image'),
+  container: document.querySelector('.container'),
+  progressBar: document.querySelector('.progress-bar'),
+  heroHeader: document.querySelector('.hero-header'),
+  contactForm: document.querySelector('.contact-form'),
+};
 
 //
 // handlers
 //
+// form event handler
+DOMElements.contactForm.addEventListener('submit', formSubmitHandler);
 // click event handler
-window.addEventListener('click', e => {
- const eventTarget = e.target;
+window.addEventListener('click', (e) => {
+  const eventTarget = e.target;
 
- if(eventTarget.matches('.whatsapp-button')) {
-  redirectWhatsapp(DOMElements);
- }
- if(eventTarget.matches('.nav-contact') || eventTarget.matches('.navbar-link-button')) {
-  scrollToContact(DOMElements);
- }
- if(eventTarget.matches('.go-top') || eventTarget.matches('footer') || eventTarget.matches('.nav-home') || eventTarget.matches('.fa-chevron-up') || eventTarget.matches('.navbar-brand-image') || eventTarget.matches('.navbar-brand-title')) {
-  scrollToTop(DOMElements);
- }
+  if (eventTarget.matches('.whatsapp-button')) {
+    redirectWhatsapp(DOMElements);
+  }
+  if (eventTarget.matches('.nav-contact') || eventTarget.matches('.navbar-link-button')) {
+    scrollToContact(DOMElements);
+  }
+  if (eventTarget.matches('.go-top') || eventTarget.matches('footer') || eventTarget.matches('.nav-home') || eventTarget.matches('.fa-chevron-up') || eventTarget.matches('.navbar-brand-image') || eventTarget.matches('.navbar-brand-title')) {
+    scrollToTop(DOMElements);
+  }
 });
 // scroll event handler
-window.addEventListener('scroll', e => {
- wAppAndNavStyling(DOMElements);
- handleProgressBar(DOMElements);
+window.addEventListener('scroll', (e) => {
+  wAppAndNavStyling(DOMElements);
+  handleProgressBar(DOMElements);
 });
 
 // resize event handler
-window.addEventListener('resize', e => {
- if(window.innerHeight > 820) {
-  fixServicesHeight(DOMElements);
- }
-})
+window.addEventListener('resize', (e) => {
+  if (window.innerHeight > 820) {
+    fixServicesHeight(DOMElements);
+  }
+});
+
 //
 // functions
 //
 function redirectWhatsapp() {
- const num = 556699331539;
- const url = 'https://api.whatsapp.com/send?phone=';
- window.open(url + num, '__blank');
+  const num = 556699331539;
+  const url = 'https://api.whatsapp.com/send?phone=';
+  window.open(url + num, '__blank');
 }
 
 function scrollToContact(DOMElements) {
- const { contactSection } = DOMElements;
- contactSection.scrollIntoView({
-  behavior: 'smooth'
- });
+  const { contactSection } = DOMElements;
+  contactSection.scrollIntoView({
+    behavior: 'smooth',
+  });
 }
 function scrollToTop(DOMElements) {
- const { heroContainer } = DOMElements;
- heroContainer.scrollIntoView({
-  behavior: 'smooth'
- })
+  const { heroContainer } = DOMElements;
+  heroContainer.scrollIntoView({
+    behavior: 'smooth',
+  });
 }
 
 function wAppAndNavStyling(DOMElements) {
- const { whatsAppButton, heroContainer, navbar, navbarBrandTitle, heroHeader } = DOMElements;
- const STATIC_TRIGGER_HEIGHT = 10;
- const triggerHeight = heroContainer.offsetHeight - heroHeader.offsetHeight;
+  const {
+    whatsAppButton, heroContainer, navbar, navbarBrandTitle, heroHeader,
+  } = DOMElements;
+  const STATIC_TRIGGER_HEIGHT = 10;
+  const triggerHeight = heroContainer.offsetHeight - heroHeader.offsetHeight;
 
- if (window.pageYOffset >= STATIC_TRIGGER_HEIGHT) {
-  whatsAppButton.style.transform = 'translateX(0)';
-  navbar.style.background = 'var(--translucent)';
-  if(window.scrollY >= triggerHeight) {
-    navbarBrandTitle.style.opacity = 1;
+  if (window.pageYOffset >= STATIC_TRIGGER_HEIGHT) {
+    whatsAppButton.style.transform = 'translateX(0)';
+    navbar.style.background = 'var(--translucent)';
+    if (window.scrollY >= triggerHeight) {
+      navbarBrandTitle.style.opacity = 1;
+    } else {
+      navbarBrandTitle.style.opacity = 0;
+    }
   } else {
-    navbarBrandTitle.style.opacity = 0;
+    whatsAppButton.style.transform = 'translateX(120%)';
+    navbar.style.background = 'var(--transparent)';
   }
- } else {
-  whatsAppButton.style.transform = 'translateX(120%)';
-  navbar.style.background = 'var(--transparent)';
- }
 }
 function fixServicesHeight(DOMElements) {
- const { serviceDescription } = DOMElements;
- const heights = serviceDescription.map(container => container.offsetHeight);
- const maxHeight = heights.sort( (a, b) => b-a)[0];
+  const { serviceDescription } = DOMElements;
+  const heights = serviceDescription.map((container) => container.offsetHeight);
+  const maxHeight = heights.sort((a, b) => b - a)[0];
 
- serviceDescription.forEach(container => {
-  if(container.offsetHeight < maxHeight) {
-   container.style.height = maxHeight + 'px';
-  }
- });
-
+  serviceDescription.forEach((container) => {
+    if (container.offsetHeight < maxHeight) {
+      container.style.height = `${maxHeight}px`;
+    }
+  });
 }
 
 function handleProgressBar(DOMElements) {
@@ -100,5 +103,18 @@ function handleProgressBar(DOMElements) {
   const containerHeight = container.clientHeight - window.innerHeight;
   const offset = window.scrollY;
 
-  progressBar.style.width = `${(offset / containerHeight) * 100}%`
+  progressBar.style.width = `${(offset / containerHeight) * 100}%`;
+}
+
+async function formSubmitHandler(e) {
+  e.preventDefault();  
+  const { contactApiUrl } = config;
+
+  const response = await fetch(contactApiUrl, {
+    method: 'POST',
+    body: {}
+  });
+
+  console.log(response)
+  
 }

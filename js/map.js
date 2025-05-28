5d4d0b8aaeabd1503240f80f9dcd87fb2b74071d
Samir Gannem <samir.garcia@tcsindustrial.com.br>
2025-05-28 15:16:24 -0400

    Alterando endereco

--- js/map.js
+++ js/map.js
@@ -4,10 +4,9 @@ const mapEl = document.querySelector('.map');
 
 function initMap() {
  const location = {
-  lat:  -15.559750,
-  lng: -55.159126
+  lat:  -15.550027,
+  lng: -55.164853
  };
-
  const map = new google.maps.Map(mapEl, {
   zoom: 16,
   center: location
